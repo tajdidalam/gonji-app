84a612a4ba88ea894aad631822466642d2e00853
tajdidalam <55346157+tajdidalam@users.noreply.github.com>
2022-06-23 15:41:33 +0600

    added the cart functionality and cart icon count with open and close cart option

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./product-card.styles.scss";
 
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -12,7 +20,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add To Cart</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add To Cart
+      </Button>
     </div>
   );
 };
